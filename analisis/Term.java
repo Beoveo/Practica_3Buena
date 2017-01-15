40830f5af11e3ee67f3d53c3b27b926aad929ea7
Ture2 <alberto_tureganocsb@hotmail.com>
2017-01-15 20:41:04 +0100

    Update Term.java

--- analisis/Term.java
+++ analisis/Term.java
@@ -1,9 +1,10 @@
 package analisis;
 
 import bytecodes.ByteCode;
+import exception.ArrayException;
 import generate_bc.Compiler;
 
 public interface Term {
 	Term parse(String term);
-	ByteCode compile(Compiler compiler);
+	ByteCode compile(Compiler compiler) throws ArrayException;
 }

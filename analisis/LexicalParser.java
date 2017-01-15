5224b9aebe3d464dd7bb542a8875644f963bf5d4
Ture2 <alberto_tureganocsb@hotmail.com>
2017-01-15 20:39:18 +0100

    Update LexicalParser.java

--- analisis/LexicalParser.java
+++ analisis/LexicalParser.java
@@ -2,20 +2,38 @@ package analisis;
 
 import instruction.Instruction;
 import instruction.InstructionParser;
+import exception.ArrayException;
 import exception.LexicalAnalisisException;
 import program.ParsedProgram;
 import program.SourceProgram;
 
-
+/**
+ * CLASS LEXICALPARSER	
+ * Esta clase se encarga de generar el Parsed program a partir del SourceProgram. 
+ * Llevara un int para saber que ejecucion se esta parseando.
+ * 
+ */
 
 public class LexicalParser {
 	private SourceProgram sProgram;
 	private int programCounter;
 	
+	/**
+	 * La constructora inicializa los valores sProgram y el contador a 0
+	 * @param scProg  SourceProgram.
+	 */
 	public LexicalParser(SourceProgram scProg){
 		this.sProgram = scProg;
 		this.programCounter = 0;
 	}
+	/**
+	 * Este es el metodo más importante de la clase. 
+	 * Se encarga de parsear el programa hasta la palabra que indique el parametro StopKey
+	 * @param pProgram	ParsedProgram del main al que se añadiran las intrucciones.
+	 * @param stopKey	Palabra para finalizar el analisis
+	 * @throws LexicalAnalisisException
+	 * @throws ArrayException
+	 */
 	 
 	public void lexicalParser(ParsedProgram pProgram, String stopKey)
 	throws LexicalAnalisisException {

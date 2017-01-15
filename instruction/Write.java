d4250a0150a0417b95dd4ce043d0bf8b310b2009
Ture2 <alberto_tureganocsb@hotmail.com>
2017-01-15 21:15:58 +0100

    Update Write.java

--- instruction/Write.java
+++ instruction/Write.java
@@ -2,6 +2,7 @@ package instruction;
 
 
 import exception.ArrayException;
+import exception.LexicalAnalisisException;
 import generate_bc.Compiler;
 import bytecodes.Load;
 import bytecodes.Out;
@@ -17,20 +18,20 @@ public class Write implements Instruction {
 	}
 
 	@Override
-	public Instruction lexParse(String[] words, LexicalParser lexParser) {
+	public Instruction lexParse(String[] words, LexicalParser lexParser) throws LexicalAnalisisException {
 		String varName;
 		if(words.length != 2) return null;
 		else{
-			if(!words[0].equalsIgnoreCase("write")) return null;
+			if(!words[0].equalsIgnoreCase("write"))return null;
 			else {
 				varName = words[1];
-				if (varName.length() !=1) return null;
+				if (varName.length() !=1) throw new LexicalAnalisisException("Error: tamaño de la variable erroneo.");
 				 else {
 					 char name = varName.charAt(0);
 					 if ('a' <= name && name <= 'z') {
 						 lexParser.increaseProgramCounter();
 						 return new Write(varName); 
-					 } else return null;
+					 } else throw new LexicalAnalisisException("Error: la variable debe de ser una letra entre la a y la z.");
 				 }
 			}
 		}

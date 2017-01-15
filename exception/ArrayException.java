dd069e6e60a3828ecf3f2ea830851c48523a1b8e
Ture2 <alberto_tureganocsb@hotmail.com>
2017-01-15 20:59:48 +0100

    Update ArrayException.java

--- exception/ArrayException.java
+++ exception/ArrayException.java
@@ -3,4 +3,5 @@ package exception;
 @SuppressWarnings("serial")
 public class ArrayException extends Exception {
 	public ArrayException(){ super();}
+	public ArrayException(String text){super(text);}
 }

c39f47085ec9b557e1c68d0d3dad59be6c1e6519
Ture2 <alberto_tureganocsb@hotmail.com>
2017-01-15 21:13:41 +0100

    Update Instruction.java

--- instruction/Instruction.java
+++ instruction/Instruction.java
@@ -7,6 +7,6 @@ import generate_bc.Compiler;
 
 
 public interface Instruction {
-	Instruction lexParse(String[] words, LexicalParser lexParser) throws LexicalAnalisisException;
+	Instruction lexParse(String[] words, LexicalParser lexParser) throws LexicalAnalisisException, ArrayException;
 	void compile(Compiler compiler) throws ArrayException;
 }
